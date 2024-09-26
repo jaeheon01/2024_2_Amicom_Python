bdc42d77677d1831eb8d167de7a4457ddad40503
JeongYHgit config --global user.email minions0579@ajou.ac.krgit config --global user.name JeongYH <minions0579@ajou.ac.kr>
2024-09-26 16:56:25 +0900

    yeonho(2)

---
+++ 정연호/practice.py
@@ -0,0 +1,13 @@
+from random import *
+주민등록번호 = "030303-3165011"
+# my_str = 주민등록번호[7:]
+# print(my_str)
+# password = str(my_str[-2:])
+# print(password)
+my_str1 = 주민등록번호[7]
+print(not(my_str1 == 3))
+
+
+
+
+
